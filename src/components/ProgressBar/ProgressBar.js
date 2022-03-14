9f36141856d6407ae4befd56713eab102511efc5
alex <probesalesman@gmail.com>
2022-03-14 19:58:42 -0400

    changes to my ProgressBar component after watching solution video

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,59 +1,80 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import styled from "styled-components";
-// import { ProgressPlugin } from "webpack";
 
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
-const BAR_SIZES = {
-  small: { "--height": "8px", "--borderRadius": "4px" },
-  medium: { "--height": "12px", "--borderRadius": "4px" },
-  large: { "--height": "16px", "--borderRadius": "4px" },
-};
-
-const WRAPPER_SIZES = {
-  small: { "--borderRadius": "2px" },
-  medium: { "--borderRadius": "4px" },
-  large: { "--borderRadius": "8px" },
+const STYLES = {
+  small: { height: 8, padding: 0, radius: 4 },
+  medium: { height: 12, padding: 0, radius: 4 },
+  large: { height: 16, padding: 4, radius: 8 },
 };
 
 const ProgressBar = ({ value, size }) => {
-  const barStyles = BAR_SIZES[size];
-  const wrapperStyles = WRAPPER_SIZES[size];
-  console.log("bar sizes?", barStyles, "wrapper sizes", wrapperStyles);
+  const styles = STYLES[size];
+
+  // if an invalid size is passed (did not find size specified in our STYLES object) then throw an exception
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
   return (
     <Wrapper
       role="progressbar"
       aria-valuenow={value}
       aria-valuemin="0"
       aria-valuemax="100"
-      style={wrapperStyles}
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
       size={size}
     >
-      <Bar value={value} style={barStyles} />
+      {/* 
+        adding VisuallyHidden component to try to cover multiple types of screen readers.
+        maybe NVDA or JAWS screenreaders would not parse aria-valuenow, aria-valuemin, etc.
+      */}
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          value={value}
+          style={{
+            "--width": value + "%",
+            "--height": styles.height + "px",
+          }}
+        />
+      </BarWrapper>
     </Wrapper>
   );
 };
 
+// I did not have the background-color set. Josh points it out in solution vid
 const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border-radius: var(--borderRadius);
-  padding: ${(props) => {
-    return props.size === "large" ? "4px" : undefined;
-  }};
+  border-radius: var(--radius);
+  padding: var(--padding);
+  /* Per solution vid: trim off corners (of inner bar) when near full - overflow: hidden; started off being applied to Wrapper, but later on in solution vid
+     it gets applied to newly-created BarWrapper component to handle truncating the Bar component at the "large" size
+  overflow: hidden;
+  */
+`;
+
+// this component is used to wrap the Bar component so that for the "large" size ProgressBar, the inner Bar component
+// gets rounded off when we approach a completion of 100% (without this, the Bar is just square because of extra padding added
+// by Wrapper when it is large. The 'overflow: hidden' property is what is used to truncate the inner Bar when nearing 100% completion)
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Per solution vid: trim off corners (of inner bar) when near full */
+  overflow: hidden;
 `;
 
 const Bar = styled.div`
-  width: ${(props) => props.value + "%"};
+  width: var(--width);
   background-color: ${COLORS.primary};
   height: var(--height);
-  border-top-left-radius: var(--borderRadius);
-  border-bottom-left-radius: var(--borderRadius);
-  border-top-right-radius: ${(props) =>
-    props.value > "99" ? "var(--borderRadius)" : undefined};
-  border-bottom-right-radius: ${(props) =>
-    props.value > "99" ? "var(--borderRadius)" : undefined};
+  border-radius: 4px 0 0 4px;
 `;
 
 export default ProgressBar;
